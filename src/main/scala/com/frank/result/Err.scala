764c8948f441ef5a094d692df9ab90a902b73100
Frank <weryutie@outlook.com>
2021-02-06 09:08:25 +0800

    fix bugs

--- src/main/scala/com/frank/result/Err.scala
+++ src/main/scala/com/frank/result/Err.scala
@@ -1,38 +1,43 @@
 package com.frank.result
 
 /** @constructor 用x创建一个Err
-  * @param x 该Err所包含的值
-  * @tparam E x的类型
-  */
+ * @param x 该Err所包含的值
+ * @tparam E x的类型
+ */
 final case class Err[T, E](x: E) extends AnyVal with Result[T, E] {
 
   /** 同[[scala.util.Either]]中的map方法
-    * @param f 函数
-    * @tparam U 返回
-    * @return result
-    */
+   *
+   * @param f 函数
+   * @tparam U 返回
+   * @return result
+   */
   override def map[U](f: T => U): Result[U, E] =
     this.asInstanceOf[Result[U, E]]
 
   /** 如果该result为ok且f(x)为true，返回true
-    * 否则返回false
-    * @param f 函数
-    * @return
-    */
+   * 否则返回false
+   *
+   * @param f 函数
+   * @return
+   */
   def exists(f: T => Boolean): Boolean = false
 
   /** 对于该result所包含的值执行f()
-    * @param f 函数
-    */
+   *
+   * @param f 函数
+   */
   def foreach(f: T => Unit): Unit = ()
 
   /** 创建seq
-    * @return seq
-    */
+   *
+   * @return seq
+   */
   def toSeq: Seq[T] = Seq()
 
   /** 返回迭代器
-    * @return iterator
-    */
+   *
+   * @return iterator
+   */
   def iterator(): Iterator[T] = Iterator()
 }
--- src/main/scala/com/frank/result/Ok.scala
+++ src/main/scala/com/frank/result/Ok.scala
@@ -1,43 +1,43 @@
 package com.frank.result
 
 /** @constructor 用x创建一个Ok
-  * @param x 该Ok所包含的值
-  * @tparam T x的类型
-  */
+ * @param x 该Ok所包含的值
+ * @tparam T x的类型
+ */
 final case class Ok[T, E](x: T) extends AnyVal with Result[T, E] {
 
   /** 同[[scala.util.Either]]中的map方法
-    *
-    * @param f 函数
-    * @tparam U 返回
-    * @return result
-    */
+   *
+   * @param f 函数
+   * @tparam U 返回
+   * @return result
+   */
   override def map[U](f: T => U): Result[U, E] = Ok(f(x))
 
   /** 如果该result为ok且f(x)为true，返回true
-    * 否则返回false
-    *
-    * @param f 函数
-    * @return
-    */
+   * 否则返回false
+   *
+   * @param f 函数
+   * @return
+   */
   def exists(f: T => Boolean): Boolean = f(x)
 
   /** 对于该result所包含的值执行f()
-    *
-    * @param f 函数
-    */
+   *
+   * @param f 函数
+   */
   def foreach(f: T => Unit): Unit = f(x)
 
   /** 创建seq
-    *
-    * @return seq
-    */
+   *
+   * @return seq
+   */
   def toSeq: Seq[T] = Seq(x)
 
   /** 返回迭代器
-    *
-    * @return iterator
-    */
+   *
+   * @return iterator
+   */
   def iterator(): Iterator[T] = Iterator(x)
 
 }
